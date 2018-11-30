f2c3e9e21eed7e21daa638b6fef77ec54ebe7ed3
uitspitss <uitspitss@gmail.com>
2018-12-01 02:14:08 +0900

    Update crkbd

--- keyboards/crkbd/keymaps/uitspitss/keymap.c
+++ keyboards/crkbd/keymaps/uitspitss/keymap.c
@@ -52,7 +52,7 @@ enum macro_keycodes {
 #define KC_LSAD  RGB_SAD
 #define KC_LVAI  RGB_VAI
 #define KC_LVAD  RGB_VAD
-#define KC_LSMOD RGB_SMOD
+#define KC_LMOD  RGB_MOD
 #define KC_CTLTB CTL_T(KC_TAB)
 #define KC_GUIEI GUI_T(KC_LANG2)
 #define KC_ALTKN ALT_T(KC_LANG1)
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
       XXXXX,   F11,   F12,SCSTCB,SCSTFL, _____,                   LEFT,  DOWN,    UP,  RGHT, _____, XXXXX,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-      XXXXX,   RST,  LTOG, LSMOD,  LHUI,  LHUD,                   LVAI,  LVAD,  LSAI,  LSAD, _____, XXXXX,\
+      XXXXX,   RST,  LTOG,  LMOD,  LHUI,  LHUD,                   LVAI,  LVAD,  LSAI,  LSAD, _____, XXXXX,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
                                   _____, _____, _____,    _____, _____, _____ \
                               //`--------------------'  `--------------------'
