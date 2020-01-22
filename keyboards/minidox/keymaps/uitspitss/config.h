3c4f9ef90d3104421442ade6e19ff0b6e7116406
uitspitss <3781626+uitspitss@users.noreply.github.com>
2020-01-23 06:24:49 +0900

    Update TAPPING_TERM

--- keyboards/minidox/keymaps/uitspitss/config.h
+++ keyboards/minidox/keymaps/uitspitss/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define PREVENT_STUCK_MODIFIERS
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 70
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D7
