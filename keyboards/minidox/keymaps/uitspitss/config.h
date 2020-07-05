5e1b6bcece5a943a82684955c37b3e09b595e7eb
uitspitss <3781626+uitspitss@users.noreply.github.com>
2020-07-05 15:37:52 +0900

    Fix

--- keyboards/minidox/keymaps/uitspitss/config.h
+++ keyboards/minidox/keymaps/uitspitss/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D7
 #define RGBLIGHT_TIMER
-#define RGBLED_NUM 12  // Number of LEDs
+#define RGBLED_NUM 6  // Number of LEDs
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
